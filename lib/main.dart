eaee9168bee916034ab8957eb6df655b8b734c41
Maduz0097 <aymperera@gmail.com>
2021-05-17 11:16:54 +0530

    debug banner false

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Face Mask Detection',
+      debugShowCheckedModeBanner: false,
       home: MySplashPage(),
     );
   }
